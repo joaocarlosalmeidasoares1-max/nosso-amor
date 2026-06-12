194c2bd7d29fd836d5de5b8ff1ea5eb2cf65eaa9
joaocarlosalmeidasoares1-max <joaocarlosalmeidasoares1@gmail.com>
2026-06-12 18:17:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const musica = document.getElementById("musica");
+const botao = document.getElementById("startBtn");
 
+botao.addEventListener("click", () => {
+
+    musica.play();
+
+    document
+    .getElementById("capitulos")
+    .scrollIntoView({
+        behavior:"smooth"
+    });
+
+});
