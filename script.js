80776e9b429adf4364fc2dc6b43064f60e9950f2
joaocarlosalmeidasoares1-max <joaocarlosalmeidasoares1@gmail.com>
2026-06-12 19:20:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ botao.addEventListener("click", () => {
     });
 
 });
+function abrirModal(imagem,titulo,texto){
+
+    document.getElementById("modal")
+    .style.display = "flex";
+
+    document.getElementById("modal-img")
+    .src = imagem;
+
+    document.getElementById("modal-titulo")
+    .innerText = titulo;
+
+    document.getElementById("modal-texto")
+    .innerText = texto;
+}
+
+function fecharModal(){
+
+    document.getElementById("modal")
+    .style.display = "none";
+}
