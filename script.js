5d11a7fdc4c8d96adeed891e47a80cdad6cbb380
joaocarlosalmeidasoares1-max <joaocarlosalmeidasoares1@gmail.com>
2026-06-12 20:05:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -36,13 +36,21 @@ botaoMusica.addEventListener("click", () => {
 });
 
 // Modal
-function abrirModal(imagem, titulo, texto) {
+function abrirModal(imagem, titulo, texto){
 
-    document.getElementById("modal").style.display = "flex";
+    alert("Modal abriu!");
 
-    document.getElementById("modal-img").src = imagem;
-    document.getElementById("modal-titulo").innerText = titulo;
-    document.getElementById("modal-texto").innerText = texto;
+    document.getElementById("modal")
+    .style.display = "flex";
+
+    document.getElementById("modal-img")
+    .src = imagem;
+
+    document.getElementById("modal-titulo")
+    .innerText = titulo;
+
+    document.getElementById("modal-texto")
+    .innerText = texto;
 }
 
 function fecharModal() {
